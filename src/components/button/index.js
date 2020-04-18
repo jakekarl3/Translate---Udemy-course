803f0af388c8b2148e888b3fe8202f279bc39c0e
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-04-18 13:35:05 -0500

    Add LanguageContext.Provider to app.js

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -5,10 +5,11 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    console.log(this.context);
+    const text = this.context === 'english' ? 'Submit' : 'voorleggen';
+
     return (
       <button className="ui button primary">
-        Submit
+        {text}
       </button>
     );
   }

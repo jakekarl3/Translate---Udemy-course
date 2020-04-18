791d763f308b3dfbe97e5df07ef6b69f035f9bb9
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-04-18 11:52:23 -0500

    Create userCreate, Field, Button component with initial setup and import

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from '../userCreate';
 
 class App extends React.Component {
   state = { language: 'english' };
@@ -15,6 +16,7 @@ class App extends React.Component {
           <i className="flag us" onClick={() => this.onLanguageChange('english')} />
           <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
         </div>
+        <UserCreate />
       </div>
     );
   }
